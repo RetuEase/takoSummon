9b95c53e988375b860ab6b440dbd3bc8d90624dc
RetuEase <1099177812@qq.com>
2022-10-29 18:19:49 +0900

    arrange Ctrler index

---
+++ scripts/controllers/ctrler.js
@@ -0,0 +1,13 @@
+/**
+ * 向监听器们提供回调函数
+ */
+
+// userOpera
+import * as UserCmd from './basicCmd/userCmdCtrler.js';
+
+// userAct
+import * as _TakoTest from '../apps/_TakoTest.js';
+
+// userQuery
+
+export default { UserCmd, _TakoTest };
